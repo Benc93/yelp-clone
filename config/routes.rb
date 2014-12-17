5bda569d4682713d0f4dd9bbc858ca782c51654a
Ben Conway <bendavidconway@me.com>
2014-12-17 14:07:11 +0000

    omniAuth setup guide finished - can now be directed to FB

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@
 Rails.application.routes.draw do
 
   root to: "restaurants#index"
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do
     resources :reviews
-  end 
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
